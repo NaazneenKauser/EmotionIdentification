9dd6ee40f1fc054c358db97e027648215768dd71
NaazneenKauser <naazneen.kauser@gmail.com>
2020-01-07 14:49:04 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+# Emotional-Identification
+https://www.kaggle.com/c/challenges-in-representation-learning-facial-expression-recognition-challenge/data
+
+
+Above is the link for dataset, download the dataset. 
+save it in folder with "dataset.csv" as its file name
+
+now create a folder
+Image
+which consits of sub-folders as
+1)test
+2)train
+
+then in both folders add these folders  
+1)Angry
+2)Disgrace
+3)Fear
+4)Happy
+5)Neutral
+6)Sad
+7)Surprise
+
+
+NOW RUN this file pixel_to_image.py file
+it fills all the photos with proper Images into respective folder
+
+then execute main.py  
+
+it has accuracy rate of 75.94%
---
+++ pixel_To_Image.py
@@ -0,0 +1,36 @@
+
+import numpy as np
+import pandas as pd
+import cv2
+import os
+
+data=pd.read_csv('dataset.csv')
+pixels=data['pixels'].tolist()
+width,height=48,48
+faces=[]
+emoj=data['emotion'].tolist()
+typ=data['Usage'].tolist()
+
+for x in pixels:
+    face=[int(j) for j in x.split(' ')]
+    face=np.asarray(face).reshape(width,height)
+    a=(48,48)
+    faces.append(face)
+faces = np.asarray(faces)
+emotions = emoj
+
+
+d={0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0}
+emo=['Angry/','Disgrace/','Fear/','Happy/','Neutral/','Sad/','Surprise/']
+cls=['test','train']
+for i in range(len(emotions)):
+        get=emotions[i]
+        #print('visited')
+        val=str(d[emotions[i]])
+        if(typ[i]=='Training'):
+            cv2.imwrite('Images/train/'+emo[get]+val+'.jpg',faces[i])
+            d[emotions[i]]=d[emotions[i]]+1
+        else:
+            cv2.imwrite('Images/test/'+emo[get]+val+'.jpg',faces[i])
+            d[emotions[i]]=d[emotions[i]]+1            
+print(d)
